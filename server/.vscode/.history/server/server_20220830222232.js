a87c3e01e07be2643ed047b6690e41536ab2f81a
jogabi <rkql1109@naver.com>
2022-08-30 22:24:17 +0900

    react 연동

---
+++ server/.vscode/.history/server/server_20220830222232.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.listen(8080, function () {
+  console.log('listening on 8080')
+});
+
+
+app.use(express.static(path.join(__dirname, '/react-project/build')));
+
+app.get('/', function (요청, res) {
+  res.sendFile(path.join(__dirname, '/react-project/build/index.html'));
+});
+
+
