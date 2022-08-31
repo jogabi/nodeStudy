83daeaf0abdd7144996f25a9f1612cfc93007afb
jogabi <rkql1109@naver.com>
2022-08-31 19:17:07 +0900

    mongodb 추가

--- app/server.js
+++ app/server.js
@@ -3,12 +3,20 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 
-/* form 데이터를 전송 body-parser */
 
-app.listen(8080, () => {
-  console.log('listening on 8080')
+/* mongo db */
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb+srv://gabi:1234@cluster0.daut0.mongodb.net/?retryWrites=true&w=majority', function (error, client) {
+  if (error) return console.log(error);
+  //서버띄우는 코드 여기로 옮기기
+  app.listen('8080', function () {
+    console.log('listening on 8080')
+  });
 })
 
+
+/* form 데이터를 전송 body-parser */
 app.get('/pet', (req, res) => {
   res.send('펫용품 테스트')
 
@@ -28,3 +36,5 @@ app.post('/add', (req, res) => {
   console.log(req.body);
   res.send("전송완료")
 })
+
+
