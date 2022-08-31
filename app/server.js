94ffa931e44cd24f8d14f00d284e6402553e4164
jogabi <rkql1109@naver.com>
2022-08-31 18:35:38 +0900

    괄호수정

--- app/server.js
+++ app/server.js
@@ -3,21 +3,21 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(8080, function () {
+app.listen(8080, () => {
   console.log('listening on 8080')
 })
 
-app.get('/pet', function (req, res) {
+app.get('/pet', (req, res) => {
   res.send('펫용품 테스트')
 
 })
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 
 })
 
-app.get('/write', function (req, res) {
+app.get('/write', (req, res) => {
   res.sendFile(__dirname + '/write.html')
 
 })
