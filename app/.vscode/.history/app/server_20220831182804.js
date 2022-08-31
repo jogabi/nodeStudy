140076d27f7b35d7e9c130ef5cef29ff13b22a12
jogabi <rkql1109@naver.com>
2022-08-31 18:28:17 +0900

    get 추가

---
+++ app/.vscode/.history/app/server_20220831182804.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+
+app.listen(8080, function () {
+  console.log('listening on 8080')
+})
+
+app.get('/pet', function (req, res) {
+  res.send('펫용품 테스트')
+
+})
