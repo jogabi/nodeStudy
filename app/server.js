9fc1b20d614826246ac3eb8dd039d6c046d227b3
jogabi <rkql1109@naver.com>
2022-08-31 19:26:30 +0900

    ejs 추가

--- app/server.js
+++ app/server.js
@@ -3,18 +3,22 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 
+var db;
 
 /* mongo db */
 const MongoClient = require('mongodb').MongoClient;
 
+app.set('view engine', 'ejs');
+
 MongoClient.connect('mongodb+srv://gabi:1234@cluster0.daut0.mongodb.net/?retryWrites=true&w=majority', function (error, client) {
   if (error) return console.log(error);
+  db = client.db('todoapp');
+
   //서버띄우는 코드 여기로 옮기기
   app.listen('8080', function () {
     console.log('listening on 8080')
   });
 })
-
 
 /* form 데이터를 전송 body-parser */
 app.get('/pet', (req, res) => {
@@ -33,8 +37,12 @@ app.get('/write', (req, res) => {
 
 /* 글쓰기 전송받음 */
 app.post('/add', (req, res) => {
-  console.log(req.body);
-  res.send("전송완료")
+  console.log(req.body.title);
+  console.log(req.body.date);
+
+  db.collection('post').insertOne({ title: req.body.title, date: req.body.date }, function () {
+    res.send("전송완료")
+  })
 })
 
 
