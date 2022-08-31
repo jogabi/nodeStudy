5f2fc131921348d45f88a83b6b40ce70f286683d
jogabi <rkql1109@naver.com>
2022-08-31 19:03:17 +0900

    write 추가

--- app/server.js
+++ app/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 
+/* form 데이터를 전송 body-parser */
+
 app.listen(8080, () => {
   console.log('listening on 8080')
 })
@@ -19,5 +21,10 @@ app.get('/', (req, res) => {
 
 app.get('/write', (req, res) => {
   res.sendFile(__dirname + '/write.html')
+})
 
+/* 글쓰기 전송받음 */
+app.post('/add', (req, res) => {
+  console.log(req.body);
+  res.send("전송완료")
 })
