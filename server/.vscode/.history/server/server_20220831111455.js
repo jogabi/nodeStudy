ee580aae8672a92a760b20802f4feb0e10b02f0b
jogabi <rkql1109@naver.com>
2022-08-31 11:18:43 +0900

    index 추가

---
+++ server/.vscode/.history/server/server_20220831111455.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+
+
+app.listen('8080', function () {
+  console.log('listening on 8080')
+})
+
+app.get('/pet', function (req, res) {
+  res.send('펫용품 사기 ')
+})
+
+
+app.get('/beauty', function (req, res) {
+  res.send('뷰티용품 사기')
+
+
+})
+
---
+++ server/server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const app = express();
+
+
+app.listen('8080', function () {
+  console.log('listening on 8080')
+})
+
+app.get('/pet', function (req, res) {
+  res.send('펫용품 사기 ')
+})
+
+
+app.get('/beauty', function (req, res) {
+  res.send('뷰티용품 사기')
+})
+
+app.get('/', function (req, res) {
+
+  res.sendFile(__dirname + '/index.html')
+
+
+})
+
