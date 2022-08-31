ea0a0454f3e12b196562a71d5b0340823cbee1b7
jogabi <rkql1109@naver.com>
2022-08-31 11:20:50 +0900

    history 삭제

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ app.get('/beauty', function (req, res) {
   res.send('뷰티용품 사기')
 })
 
+app.get('/write', function (req, res) {
+  res.sendFile(__dirname + 'write.html')
+})
+
 app.get('/', function (req, res) {
 
   res.sendFile(__dirname + '/index.html')
