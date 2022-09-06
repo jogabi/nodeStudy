2d1def00eb68a20474a7dc3faf9f43a176baff1b
jogabi <rkql1109@naver.com>
2022-09-06 22:42:33 +0900

    내용수정 추가

--- app/server.js
+++ app/server.js
@@ -9,14 +9,17 @@ var db;
 /* mongo db */
 const MongoClient = require('mongodb').MongoClient;
 
+/* put수정요청 */
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 
-MongoClient.connect('mongodb+srv://gabi:1234@cluster0.daut0.mongodb.net/?retryWrites=true&w=majority', function (error, client) {
+MongoClient.connect('mongodb+srv://gabi:1234@cluster0.daut0.mongodb.net/?retryWrites=true&w=majority', (error, client) => {
   if (error) return console.log(error);
   db = client.db('todoapp');
 
   //서버띄우는 코드 여기로 옮기기
-  app.listen('8080', function () {
+  app.listen('8080', () => {
     console.log('listening on 8080')
   });
 })
@@ -24,7 +27,6 @@ MongoClient.connect('mongodb+srv://gabi:1234@cluster0.daut0.mongodb.net/?retryWr
 /* form 데이터를 전송 body-parser */
 app.get('/pet', (req, res) => {
   res.send('펫용품 테스트')
-
 })
 
 app.get('/', (req, res) => {
@@ -33,30 +35,31 @@ app.get('/', (req, res) => {
 
 app.get('/write', (req, res) => {
   res.render('write.ejs')
-
 })
 
 /* 글쓰기 전송받음 */
 app.post('/add', (req, res) => {
 
-  db.collection('counter').findOne({ name: '게시물갯수' }, function (error, result) {
+  db.collection('counter').findOne({ name: '게시물갯수' }, (error, result) => {
     let resultPost = parseInt(result.totalPost)
-    db.collection('post').insertOne({ id: resultPost + 1, title: req.body.title, date: req.body.date }, function () {
+    db.collection('post').insertOne({ id: resultPost + 1, title: req.body.title, date: req.body.date }, () => {
 
-      db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, function (error, result) {
+      db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, (error, result) => {
         console.log('total couter 수정 완료');
       })
     })
-    res.send("전송완료")
+    res.render('write.ejs')
+
+
   })
+
 })
 
 /* delete 요청 들어올떄 삭제 */
-app.delete('/delete', function (req, res) {
+app.delete('/delete', (req, res) => {
   req.body.id = parseInt(req.body.id)
-
   /* db에서 post 요청을 할 때 deleteOne 을 해줌 */
-  db.collection('post').deleteOne(req.body, function (error, result) {
+  db.collection('post').deleteOne(req.body, (error, result) => {
     console.log('삭제완료');
   })
   res.send('삭제완료')
@@ -64,19 +67,27 @@ app.delete('/delete', function (req, res) {
 
 
 /* list 꺼내기 */
-app.get('/list', function (req, res) {
-  db.collection('post').find().toArray(function (error, result) {
+app.get('/list', (req, res) => {
+  db.collection('post').find().toArray((error, result) => {
     /* ejs 파일 보내기 */
     res.render('list.ejs', { posts: result })
   })
 })
 
 /* edit 상세페이지 */
-app.get('/edit/:id', function (req, res) {
+app.get('/edit/:id', (req, res) => {
   /* post 에서 findOne 해서 id parmas 가져옴 */
-
-  db.collection('post').findOne({ id: parseInt(req.params.id) }, function (error, result) {
-    res.render('edit.ejs', { data: result })
+  db.collection('post').findOne({ id: parseInt(req.params.id) }, (error, result) => {
+    res.render('edit.ejs', { posts: result })
   })
+})
 
+/* 수정하기 */
+
+app.put('/edit', function (req, res) {
+  console.log(req.body);
+  db.collection('post').updateOne({ id: parseInt(req.body.id) }, { $set: { title: req.body.title, date: req.body.date } }, function () {
+    console.log('수정완료');
+    res.redirect('/list')
+  })
 })
